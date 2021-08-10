5ece81b7f5b0b3b2a50888599c8cc102d2ba7e6b
Ubuntu <ubuntu@ip-172-31-60-181.ec2.internal>
2021-08-10 01:26:00 +0000

    first commit

---
+++ saxpy.cu
@@ -0,0 +1,76 @@
+#include <iostream>
+#include "assert.h"
+
+
+#define WORK_PER_THREAD 16
+
+__global__ void saxpy_parallel(int n, float *x, float *y)
+{
+	int i = blockIdx.x*blockDim.x + threadIdx.x;
+	i *= WORK_PER_THREAD;
+	
+	#pragma unroll
+	for(int j=0; j<WORK_PER_THREAD; j++)
+		y[i+j] = x[i+j] + y[i+j];
+}
+
+
+void checkCUDAError(const char *msg)
+{
+    cudaError_t err = cudaGetLastError();
+    if( cudaSuccess != err) 
+    {
+        fprintf(stderr, "Cuda error: %s: %s.\n", msg, cudaGetErrorString( err) );
+        exit(-1);
+    }                         
+}
+
+int main()
+{
+	int N = 32 * 10000000;
+	// allocate vectors on host
+	int size = N * sizeof(float);
+	float* h_x = (float*)malloc(size);
+	float* h_y = (float*)malloc(size);
+	
+
+	// allocate device memory
+	float* d_x; float* d_y;
+
+	cudaMalloc((void**) &d_x, size);
+	cudaMalloc((void**) &d_y, size);
+
+	// put values in h_x and h_y
+
+	for (int i = 0; i<N ;i++)
+	{
+		h_x[i]= (float) i;
+		h_y[i]= (float) i;
+	}
+
+	cudaMemcpy(d_x, h_x, size, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_y, h_y, size, cudaMemcpyHostToDevice);
+
+	// calculate number of blocks needed for N 
+	int nblocks = ((N / WORK_PER_THREAD)+255)/256;
+
+	// call 
+	saxpy_parallel<<<nblocks,256>>>(N , d_x, d_y);
+	
+	// Copy results back from device memory to host memory
+	// implicty waits for threads to excute
+	cudaMemcpy(h_y, d_y, size, cudaMemcpyDeviceToHost);
+
+	for(int i = 0; i<N; i++) assert(h_y[i] == 2*i);
+
+	// Check for any CUDA errors
+  	checkCUDAError("cudaMemcpy calls");
+
+	cudaFree(d_x);
+	cudaFree(d_y);
+
+	free(h_x);
+	free(h_y);
+	return 0;
+
+}
